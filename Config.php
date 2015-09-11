420dfee1e1c8627bce07be1d2bb91c82a5f06084
ordomalleus <ksiva_god_@bk.ru>
2015-09-11 10:53:08 +0500

    фикс конфига, был по привычке добавлен в гит игнор

---
+++ Config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Config
+{
+
+    //настройки базы данных
+    public $server = '127.0.0.1';
+    public $user = 'root';
+    public $password = '';
+    public $bd = 'reactive';
+}
